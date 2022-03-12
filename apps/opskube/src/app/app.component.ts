35f5733bd0819e3496c925ec8ba6972c5fd835ab
Sanskar123456789 <sanskar.agarwal91@gmail.com>
2022-03-12 18:13:42 +0530

    initial commit

---
+++ apps/opskube/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'opskube-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'opskube';
+}
